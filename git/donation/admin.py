196e0a7054010d199f85199cd67a9154493f3a1e
adamstok <adamstokowski@outlook.com>
2020-07-17 20:17:15 +0200

    added models to admin page

--- git/donation/admin.py
+++ git/donation/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from donation.models import Category, Donation, Institution, type_choices
+
+admin.site.register(Category)
+admin.site.register(Donation)
+admin.site.register(Institution)
+
